1abda851867f3fa5ce46fc8f4bb6f7757a4a935c
magranadosb <antonio.granados_dx@outlook.com>
2021-05-23 13:41:47 -0500

    Se cambia realiza el cambio de toda la plantilla.

--- src/app/shared/sidenav/sidenav.component.ts
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,10 +8,22 @@ import { Router } from '@angular/router';
   ]
 })
 export class SidenavComponent implements OnInit {
+  private classActive: string = 'nav-link collapsed cursor-pointer';
+  private classActiveShow: string = 'nav-link cursor-pointer active';
+  private classCollapse: string = 'collapse';
+  private classCollapseShow: string = 'collapse show';
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public getClassActive(rlaConf: boolean, rlaPerU: boolean, rlaPerP: boolean) {
+    return (rlaConf || rlaPerU || rlaPerP) ? this.classActiveShow: this.classActive;
+  }
+
+  public getClassCollapse(rlaConf: boolean, rlaPerU: boolean, rlaPerP: boolean) {
+    return (rlaConf || rlaPerU || rlaPerP) ? this.classCollapseShow: this.classCollapse;
   }
 
   public logout() {
