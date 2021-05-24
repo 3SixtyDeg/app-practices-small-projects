3b5c87936a116c19e6ae6cc0fbd949dfde94373e
magranadosb <antonio.granados_dx@outlook.com>
2021-05-24 11:26:12 -0500

    Se finaliza práctica.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-admin-web-rossli';
+  title = 'app-practices-small-projects';
 }
